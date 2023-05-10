427670c6c4c9b3476d38a8ca6faef80abf8106b5
Rico Eka Rizkya <ricorizkya26@gmail.com>
2023-05-10 20:57:24 +0700

    first commit

--- puskesmas-dokter/gejala/tambah-gejala.php
+++ puskesmas-dokter/gejala/tambah-gejala.php
@@ -102,7 +102,7 @@
                         </li>
 
                         <li>
-                            <a class="dropdown-item d-flex align-items-center" href="logout.php">
+                            <a class="dropdown-item d-flex align-items-center" href="../logout.php">
                                 <i class="bi bi-box-arrow-right"></i>
                                 <span>Sign Out</span>
                             </a>
